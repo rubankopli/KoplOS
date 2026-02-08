739d37e59d5feb06bc6b0e907eaa8cdcb46276e2
Ruban Kopli <rubankopli@gmail.com>
2026-02-08 13:34:27 -0500

    Added shebang for bash to fisher-install script

--- files/system/etc/shadow-maint/useradd-post.d/fisher-install.sh
+++ files/system/etc/shadow-maint/useradd-post.d/fisher-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Install the 'fisher' plugin manager
 fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
