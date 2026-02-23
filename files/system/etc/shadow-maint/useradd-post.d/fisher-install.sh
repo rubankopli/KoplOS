697a9ac54566cfbb3f1da24b2333ff9b8d800fe5
Ruban Kopli <rubankopli@gmail.com>
2026-02-23 10:27:21 -0500

    #comment Removed skeleton files that were causing issues with useradd during install steps with ISO. Added a few fonts as TODOs

--- files/system/etc/shadow-maint/useradd-post.d/fisher-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install the 'fisher' plugin manager
-fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-# Have fisher install all plugins defined in the fish_plugins file
-fish --command="fisher update"
